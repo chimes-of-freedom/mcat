6392486f2e77023b03b6248fd4eaada47921d5b5
chimes-of-freedom <chimes-of-freedom@outlook.com>
2026-03-26 19:29:40 +0800

    feat: 添加 `Tag` 到 `TagAttributes` 的转换

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ pub mod common;
 pub mod display;
 pub mod edit;
 
+use lofty::prelude::*;
+use lofty::tag::Tag;
 use serde::{Deserialize, Serialize};
 use std::fmt;
 use std::io;
@@ -92,5 +94,16 @@ impl TagAttributes {
                 genre: None,
             }
         )
+    }
+
+    /// parse `Tag` to `TagAttributes`
+    pub fn from_tag(tag: &Tag) -> TagAttributes {
+        TagAttributes {
+            title: tag.title().as_deref().map(str::to_string),
+            artist: tag.artist().as_deref().map(str::to_string),
+            album: tag.album().as_deref().map(str::to_string),
+            album_artist: tag.get_string(ItemKey::AlbumArtist).map(str::to_string),
+            genre: tag.genre().as_deref().map(str::to_string),
+        }
     }
 }
