92fbc41da60223af7768228ebf8af9c9a790e684
chimes-of-freedom <chimes-of-freedom@outlook.com>
2026-03-26 18:11:23 +0800

    chore: 规范变量命名

--- src/edit.rs
+++ src/edit.rs
@@ -8,22 +8,22 @@ use lofty::{config::WriteOptions, tag::Tag};
 pub fn edit_tag<P: AsRef<Path>>(
     output_path: P,
     tag: &mut Tag,
-    tag_attrs: TagAttributes,
+    tag_attr: TagAttributes,
 ) -> Result<(), McatError> {
-    if tag_attrs.is_empty() {
+    if tag_attr.is_empty() {
         return Err(McatError::AttrEmpty);
     }
 
-    if let Some(title) = tag_attrs.title {
+    if let Some(title) = tag_attr.title {
         tag.set_title(title);
     }
-    if let Some(artist) = tag_attrs.artist {
+    if let Some(artist) = tag_attr.artist {
         tag.set_artist(artist);
     }
-    if let Some(album) = tag_attrs.album {
+    if let Some(album) = tag_attr.album {
         tag.set_album(album);
     }
-    if let Some(genre) = tag_attrs.genre {
+    if let Some(genre) = tag_attr.genre {
         tag.set_genre(genre);
     }
 
