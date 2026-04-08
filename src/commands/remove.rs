9ca48f41b3e2ad365aa5627b342f9fd6aca4a0c8
chimes-of-freedom <chimes-of-freedom@outlook.com>
2026-04-08 14:42:13 +0800

    feat: 实现 `remove` 子命令的 `--remove-file` 参数

--- src/commands/remove.rs
+++ src/commands/remove.rs
@@ -1,10 +1,13 @@
 //! `remove` command handler for deleting tracks from the repository.
 
+use std::fs;
+
 use mcat::{
     config,
     errors::{McatError, McatResult},
     models::TrackFilter,
     repos::{Repo, toml_repo::TomlDb},
+    services::*,
 };
 
 /// Executes the `remove` command.
@@ -21,10 +24,6 @@ pub fn execute(
     hashes: Vec<String>,
     remove_file: bool,
 ) -> McatResult<()> {
-    if remove_file {
-        todo!("crate::commands::remove::execute(): `--remove-file` not implemented yet");
-    }
-
     let mut db: TomlDb = Repo::from(config::repo_file_path())?;
     let filter = TrackFilter::new(titles, artists, albums, album_artists, genres, hashes);
 
@@ -34,8 +33,33 @@ pub fn execute(
         return Err(McatError::TrackNotFound);
     }
 
-    for file_hash in matched_hashes {
-        db.remove_track(&file_hash)?;
+    for file_hash in &matched_hashes {
+        db.remove_track(file_hash)?;
+    }
+
+    if remove_file {
+        let files = fs::read_dir(config::media_dir_path())?;
+
+        for file in files {
+            let file = file?;
+            let file_path = file.path();
+            let file_name = file.file_name();
+            let file_type = file.file_type()?;
+
+            if file_type.is_file() && is_file_supported(&file_path)? {
+                let stripped_data = strip_tags_from_file(&file_path, false)?.unwrap();
+                let file_hash = get_hash_from_vec(&stripped_data);
+                if matched_hashes.contains(&file_hash) {
+                    fs::remove_file(&file_path)?;
+                    println!(
+                        "Removed file \"{}\"",
+                        file_name
+                            .to_str()
+                            .unwrap_or("[WARNING] not a valid UTF-8 file name"),
+                    );
+                }
+            }
+        }
     }
 
     db.persist()
