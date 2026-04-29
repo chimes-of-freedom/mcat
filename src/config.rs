692f9a7d94b55c8a210b06b024083a778a7c1243
chimes-of-freedom <chimes-of-freedom@outlook.com>
2026-04-29 15:09:27 +0800

    refactor: 为 `config` 增加未来覆盖全局配置的能力

--- src/config.rs
+++ src/config.rs
@@ -1,28 +1,53 @@
 //! Path-related configuration functions of mcat.
 
 use std::path::PathBuf;
+use std::sync::OnceLock;
 
-const REPO_FILE_PATH: &str = ".mcat/repo.toml";
-const COVER_DIR_PATH: &str = ".mcat/images/";
-const MEDIA_DIR_PATH: &str = "media/";
+static CONFIG: OnceLock<Config> = OnceLock::new();
+
+#[derive(Debug)]
+pub struct Config {
+    repo_file: PathBuf,
+    cover_dir: PathBuf,
+    media_dir: PathBuf,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            repo_file: PathBuf::from(".mcat/repo.toml"),
+            cover_dir: PathBuf::from(".mcat/images/"),
+            media_dir: PathBuf::from("media/"),
+        }
+    }
+}
+
+fn get() -> &'static Config {
+    CONFIG.get_or_init(Config::default)
+}
+
+pub fn init(config: Config) {
+    CONFIG.set(config).expect("config already initialized");
+}
 
 /// Returns path to repository file.
 pub fn repo_file_path() -> PathBuf {
-    PathBuf::from(REPO_FILE_PATH)
+    get().repo_file.clone()
 }
 
 /// Returns path to repository backup file.
 pub fn repo_backup_file_path() -> PathBuf {
-    let base = PathBuf::from(REPO_FILE_PATH);
-    base.with_added_extension("bak")
+    let mut p = get().repo_file.clone();
+    p.set_extension("bak");
+    p
 }
 
 /// Returns path to cover images directory.
 pub fn cover_dir_path() -> PathBuf {
-    PathBuf::from(COVER_DIR_PATH)
+    get().cover_dir.clone()
 }
 
 /// Returns path to media directory.
 pub fn media_dir_path() -> PathBuf {
-    PathBuf::from(MEDIA_DIR_PATH)
+    get().media_dir.clone()
 }
